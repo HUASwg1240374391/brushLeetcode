8364589dda840a3a6008e9282c805fb20ff21e90
huas_wg <1240374391@qq.com>
2019-05-12 09:51:39 +0800

    add 111.二叉树的最小深度 101.对称二叉树

--- run.js
+++ run.js
@@ -1,26 +1,14 @@
-var multiply = function(str1, str2) {
-  if (!str1 || !str2) {
-    return 0
-  }
-  let arr1 = str1.split('').reverse()
-  let arr2 = str2.split('').reverse()
-  let result = []
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      let current = parseInt(arr1[i] * arr2[j])
-      let ten = Math.floor(current / 10)
-      let one = current % 10
-      console.log(`ten: ${ten} one: ${one}`)
-      if (!result[i]) {
-        result[i + j] = 0
-      }
-      let carry = (result[i + j] += one)
-      result[i + j] = carry % 10
-      result[i + j + 1] += ten + Math.floor(carry / 10)
+var minDepth = function(root) {
+  let q = [root],
+    deep = 0
+  while (q.length > 0 && root) {
+    deep++
+    for (let i = 0, len = q.length; i < len; ++i) {
+      let n = q.shift()
+      if (!n.left && !n.right) return deep
+      if (n.left) q.push(n.left)
+      if (n.right) q.push(n.right)
     }
   }
-  console.log(result)
-  return result.join('')
+  return deep
 }
-let result = multiply('123', '456')
-console.log(result)
