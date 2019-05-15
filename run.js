6f3f9b13220a5db89096d652b9c78d0aadbd04bb
huas_wg <1240374391@qq.com>
2019-05-15 22:13:39 +0800

    add 43.字符串相乘

--- run.js
+++ run.js
@@ -1,14 +1,23 @@
-var minDepth = function(root) {
-  let q = [root],
-    deep = 0
-  while (q.length > 0 && root) {
-    deep++
-    for (let i = 0, len = q.length; i < len; ++i) {
-      let n = q.shift()
-      if (!n.left && !n.right) return deep
-      if (n.left) q.push(n.left)
-      if (n.right) q.push(n.right)
+const multiply = function(num1, num2) {
+  if (!+num1 || !+num2) return '0'
+
+  const len1 = num1.length - 1
+  const len2 = num2.length - 1
+
+  const mulRes = new Array(num1.length + num2.length).fill(0)
+
+  for (let i = len1; i >= 0; i--) {
+    for (let j = len2; j >= 0; j--) {
+      let mul = num1[i] * num2[j]
+      mul += mulRes[i + j + 1]
+      mulRes[i + j] += parseInt(mul / 10)
+      mulRes[i + j + 1] = mul % 10
     }
   }
-  return deep
+
+  while (mulRes[0] === 0) {
+    mulRes.shift()
+  }
+
+  return mulRes.join('')
 }
