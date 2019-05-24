1cbc81ccfcaee21db65b985f708b492db1207bd4
huas_wg <1240374391@qq.com>
2019-05-24 10:47:42 +0800

    add 10.正则表达式匹配

--- run.js
+++ run.js
@@ -1,24 +1,3 @@
-var subsets = function(nums) {
-  let len = nums.length
-  if (len === 0) return [[]]
-
-  let result = []
-  function dfs(n, descison = []) {
-    if (descison.length === n) return result.push(descison)
-    dfs(n, descison.concat(true))
-    dfs(n, descison.concat(false))
-  }
-
-  dfs(len)
-
-  return result.map(arr => {
-    let tmp = []
-    arr.forEach((flag, index) => {
-      console.log(flag, index)
-      if (flag) tmp.push(nums[index])
-    })
-    return tmp
-  })
+const isMatch = (s, p) => {
+  return s === (s.match(new RegExp(p)) ? s.match(new RegExp(p))[0] : null)
 }
-let result = subsets([1, 2, 3])
-// console.log(result)
