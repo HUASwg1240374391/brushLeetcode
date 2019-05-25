f41e8e4042906c63b5ab9aa1ca9a5b7090cd7473
huas_wg <1240374391@qq.com>
2019-05-25 09:38:04 +0800

    add 100.相同的树

--- run.js
+++ run.js
@@ -1,3 +1,33 @@
-const isMatch = (s, p) => {
-  return s === (s.match(new RegExp(p)) ? s.match(new RegExp(p))[0] : null)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  let queue_left = [p]
+  let queue_right = [q]
+  let left = []
+  let right = []
+  var dfs = function(nodes) {
+    var result = []
+    var stack = []
+    stack.push(nodes)
+    while (stack.length) {
+      var item = stack.pop()
+      result.push(item.val)
+      item.right && stack.push(item.right)
+      item.left && stack.push(item.left)
+    }
+    return result
+  }
+  left = dfs(p)
+  right = dfs(q)
+  return left.join() === right.join()
 }
