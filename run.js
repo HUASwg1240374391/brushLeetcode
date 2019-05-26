d8a973875751f5eaf2e17ab5a744c5afde67e78d
huas_wg <1240374391@qq.com>
2019-05-26 10:16:33 +0800

    add 56.合并区间

--- run.js
+++ run.js
@@ -1,33 +1,15 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {boolean}
- */
-var isSameTree = function(p, q) {
-  let queue_left = [p]
-  let queue_right = [q]
-  let left = []
-  let right = []
-  var dfs = function(nodes) {
-    var result = []
-    var stack = []
-    stack.push(nodes)
-    while (stack.length) {
-      var item = stack.pop()
-      result.push(item.val)
-      item.right && stack.push(item.right)
-      item.left && stack.push(item.left)
-    }
-    return result
-  }
-  left = dfs(p)
-  right = dfs(q)
-  return left.join() === right.join()
+var merge = function(intervals) {
+  return intervals
+    .sort((a, b) => a[0] - b[0])
+    .reduce((a, b) => {
+      if (a.length === 0) return [b]
+      let i = a.length - 1
+      if (a[i][1] >= b[0]) {
+        if (a[i][1] > b[1]) return a
+        a[i] = [a[i][0], b[1]]
+      } else {
+        a.push(b)
+      }
+      return a
+    }, [])
 }
