ba362b39a3d5f1aafc5d94f5865566a78dd9dfb4
huas_wg <1240374391@qq.com>
2019-05-28 14:48:47 +0800

    add 83.删除排序链表中重复元素

--- run.js
+++ run.js
@@ -1,15 +1,14 @@
-var merge = function(intervals) {
-  return intervals
-    .sort((a, b) => a[0] - b[0])
-    .reduce((a, b) => {
-      if (a.length === 0) return [b]
-      let i = a.length - 1
-      if (a[i][1] >= b[0]) {
-        if (a[i][1] > b[1]) return a
-        a[i] = [a[i][0], b[1]]
-      } else {
-        a.push(b)
-      }
-      return a
-    }, [])
+var deleteDuplicates = function(head) {
+  if (!head || !head.next) {
+    return head
+  }
+  let tem = head
+  while (tem && tem.next) {
+    if (tem.val === tem.next.val) {
+      tem.next = tem.next.next
+    } else {
+      tem = tem.next
+    }
+  }
+  return head
 }
