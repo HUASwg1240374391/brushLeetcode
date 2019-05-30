00018ea89bab86de50e906dd71113fc558beb41e
huas_wg <1240374391@qq.com>
2019-05-30 08:52:29 +0800

    add 70.爬楼梯

--- run.js
+++ run.js
@@ -1,24 +1,16 @@
-var mySqrt = function(x) {
-  if (!x) {
-    return 0
-  }
-  let tem = Math.floor(0 + x) / 2
-
-  while (1) {
-    let middle = tem
-    let result = tem * tem
-    if (result >= x) {
-      if (
-        result === x ||
-        ((tem + 1) * (tem + 1) > x && (tem - 1) * (tem - 1) < x)
-      ) {
-        return tem
-      } else {
-        tem = (0 + tem) / 2
-      }
-    } else if (result < x) {
-      tem = (tem + middle) / 2
+var climbStairs = function(n) {
+    let result=[1,2]
+    if(n===0){
+      return 0
     }
-  }
-}
-console.log(mySqrt(8))
+    if(n===1){
+      return 1
+    }
+    if(n===2){
+      return 2
+    }
+    for(let i=2;i<=n;i++){
+      result[i]=result[i-1]+result[i-2]
+    }
+    return result[result.length-1]
+};
