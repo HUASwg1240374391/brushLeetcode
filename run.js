6593a3ee4e39ebcf8e2ac53471863aecaf933a76
huas_wg <1240374391@qq.com>
2019-06-25 10:16:27 +0800

    add 929.独特的电子邮件

--- run.js
+++ run.js
@@ -1,33 +1,16 @@
-function maxProduct(nums) {
-  let res = -Number.MAX_VALUE;
-  let min = 1;
-  let max = 1;
-  for (let num of nums) {
-    [min, max] = [
-      Math.min(num, min * num, max * num),
-      Math.max(num, min * num, max * num),
-    ];
-    res = Math.max(res, max);
-  }
-  return res;
-}
+var numUniqueEmails = function(emails) {
+  let set = new Set();
 
-var maxProduct = function(nums) {
-  if (nums.length < 1) return 0;
-  let max = -Infinity, min = 0, cur = 0;
-  for (let n of nums) {
-      if (n > 0) {
-          min = (min === 0 ? 0 : min * n);
-          cur = (cur <=  1 ? n : cur * n);
-      } else if (n < 0) {
-          let tempMin = min;
-          min = (cur <=  1 ? n : cur * n);
-          cur = (tempMin === 0 ? n : tempMin * n);
-      } else {
-          min = 0;
-          cur = 0;
-      }
-      max = Math.max(max, cur);
-  }
-  return max;
+  emails.forEach(email => {
+    let [local, domain] = email.split("@");
+    local = local.replace(/[.]/g, ""); // replace '.' with empty char
+    const plusIndex = local.indexOf("+");
+    if (plusIndex !== -1) {
+      local = local.substring(0, plusIndex);
+    }
+
+    set.add(`${local}@${domain}`);
+  });
+
+  return set.size;
 };
